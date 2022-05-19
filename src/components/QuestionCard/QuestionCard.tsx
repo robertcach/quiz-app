ddeb944747dffcc8ed6cc27729bcea575a9f0132
Robert Cabrera <cabrera.rch@gmail.com>
2022-05-19 14:08:21 +0200

    feat: create custom type to QuestionCard component

--- src/components/QuestionCard/QuestionCard.tsx
+++ src/components/QuestionCard/QuestionCard.tsx
@@ -1,8 +1,17 @@
-const QuestionCard = () => {
+type QuestionCardProps = {
+  question: string;
+  answer: string[];
+  callback: any;
+  userAnswer: string;
+  questionNumber: number;
+  totalQuestions: number;
+}
+
+const QuestionCard: React.FC<QuestionCardProps> = ({question, answer, callback, userAnswer, questionNumber, totalQuestions}) => {
   return (
-    <>
-      Question Card
-    </>
+    <div>
+      <p>Question: {questionNumber} / {question} </p>
+    </div>
   )
 };
 
